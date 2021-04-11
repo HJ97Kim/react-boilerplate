020e214c57e8d94ff8d3ee349391bc98a51dec52
Hyeongjin Kim <kimjin5537@daum.net>
2021-04-11 21:45:42 +0900

    Add: bodyParser, signUp, nodemon

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 6000;
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
 mongoose
@@ -13,6 +21,17 @@ mongoose
   .then(() => console.log('MongoDB 연결성공!'))
   .catch((err) => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!!!!'));
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
