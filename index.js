d6fc740699eff8fba3632df76e141157e705039d
Hyeongjin Kim <kimjin5537@daum.net>
2021-04-11 18:18:37 +0900

    DB 연결

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = 6000;
+
+const mongoose = require('mongoose');
+mongoose
+  .connect('mongodb+srv://HJ97Kim:gudwls5537@boilerplate.ppe3e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('MongoDB 연결성공!'))
+  .catch((err) => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
